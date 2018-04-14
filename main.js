3e967e086e3aed83c7209e5844474d9013d548a2
Andrew Harrington <eightbitandrew@gmail.com>
2018-04-13 20:40:36 -0700

    test

--- main.js
+++ main.js
@@ -10,36 +10,38 @@ function initializeApp(){
 }
 
 
-function getWikiInfo(){}
-$.ajax({
-    type: "GET",
-    url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Scott_Tingle&callback=?",
-    contentType: "application/json; charset=utf-8",
-    async: false,
-    dataType: "json",
-    success: function (data, textStatus, jqXHR) {
+function getWikiInfo() {
+    $.ajax({
+        type: "GET",
+        url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Scott_Tingle&callback=?",
+        contentType: "application/json; charset=utf-8",
+        async: false,
+        dataType: "json",
+        success: function (data, textStatus, jqXHR) {
 
-        var markup = data.parse.text["*"];
-        var blurb = $('<div></div>').html(markup);
+            var markup = data.parse.text["*"];
+            var blurb = $('<div></div>').html(markup);
 
-        // remove links as they will not work
-        blurb.find('a').each(function() { $(this).replaceWith($(this).html()); });
+            // remove links as they will not work
+            blurb.find('a').each(function () {
+                $(this).replaceWith($(this).html());
+            });
 
-        // remove any references
-        blurb.find('sup').remove();
+            // remove any references
+            blurb.find('sup').remove();
 
-        // remove cite error
-        blurb.find('.mw-ext-cite-error').remove();
-        var article_text=$('#article').html($(blurb).find('p'));
-        $('body').append(article_text);
+            // remove cite error
+            blurb.find('.mw-ext-cite-error').remove();
+            var article_text = $('#article').html($(blurb).find('p'));
+            $('body').append(article_text);
 
-        console.log(data);
+            console.log(data);
 
-    },
-    error: function (errorMessage) {
-    }
-});
-
+        },
+        error: function (errorMessage) {
+        }
+    });
+}
 
 
     //     $.ajax({
